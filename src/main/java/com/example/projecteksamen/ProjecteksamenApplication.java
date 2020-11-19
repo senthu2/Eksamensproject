223aad3263d5453edfeae251c8279c81f0f26957
senthu2 <56972476+senthu2@users.noreply.github.com>
2020-11-19 15:21:58 +0100

    first commit

---
+++ src/main/java/com/example/projecteksamen/ProjecteksamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecteksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecteksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjecteksamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projecteksamen/ProjecteksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecteksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecteksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
