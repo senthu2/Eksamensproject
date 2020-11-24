b4958c5e90b5f689f005cd48f2968587a8215087
senthu2 <56972476+senthu2@users.noreply.github.com>
2020-11-24 21:53:39 +0100

    first commit

--- src/main/java/com/example/projecteksamen/HomeController.java
+++ src/main/java/com/example/projecteksamen/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.projecteksamen.service;
+package com.example.projecteksamen;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
