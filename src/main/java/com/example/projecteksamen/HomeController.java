27b4aa4ca69fcc6fd505f166fdadb81a3d0fa918
senthu2 <56972476+senthu2@users.noreply.github.com>
2020-11-27 19:55:38 +0100

    first commit

--- src/main/java/com/example/projecteksamen/HomeController.java
+++ src/main/java/com/example/projecteksamen/HomeController.java
@@ -1,13 +1,20 @@
 package com.example.projecteksamen;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping
+    @RequestMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/KontaktInfo")
+    public String kontaktInfo(){
+        return "KontaktInfo";
     }
 }
