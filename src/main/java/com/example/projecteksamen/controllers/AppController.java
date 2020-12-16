b58aa6ada783ceef70951da3ff844df9967fede9
senthu2 <56972476+senthu2@users.noreply.github.com>
2020-12-16 21:55:41 +0100

    first commit

--- src/main/java/com/example/projecteksamen/controllers/AppController.java
+++ src/main/java/com/example/projecteksamen/controllers/AppController.java
@@ -1,12 +1,15 @@
 package com.example.projecteksamen.controllers;
 
+import com.example.projecteksamen.models.Reperation;
 import com.example.projecteksamen.models.User;
 import com.example.projecteksamen.repositories.UserRepository;
+import com.example.projecteksamen.services.ReperationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -16,6 +19,9 @@ public class AppController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    ReperationService reperationService;
 
 
     @GetMapping("/register")
@@ -37,6 +43,29 @@ public class AppController {
     @GetMapping("/users")
     public String listUsers() {
         return "indexManager";
+    }
+
+    @GetMapping("/reperationCreateManager")
+    public String createReperation(Model model){
+        System.out.println("Vis reperation");
+        Reperation reperation = new Reperation();
+        model.addAttribute("reperation",reperation);
+        return "reperation/reperationCreateManager";
+    }
+
+    @PostMapping("/reperationCreateManager")
+    public String createReperation(@ModelAttribute("reperation") Reperation reperation){
+        System.out.println("Opret reperation");
+        reperation.setFirst_Name(reperation.getFirst_Name());
+        reperation.setLast_Name(reperation.getLast_Name());
+        reperation.setAddress(reperation.getAddress());
+        reperation.setEmail(reperation.getEmail());
+        reperation.setPhone_Number(reperation.getPhone_Number());
+        reperation.setDamage_Location(reperation.getDamage_Location());
+        reperation.setDescription(reperation.getDescription());
+        reperationService.save(reperation);
+        return "reperation/reperationCreateManager";
+
     }
 
     @GetMapping("/omOsManager")
